6000c12118a8dbd014636b23168b1754c860d099
zoulq <zoulq@dztec.net>
2020-06-18 23:10:46 +0800

    修复未注册用户可以登录问题

--- chatServer/app/api/v1/token.js
+++ chatServer/app/api/v1/token.js
@@ -1,13 +1,13 @@
 const Router = require('koa-router')
 const { User } = require('@models/user')
 const { RegisterValidator, NotEmptyValidator } = require('@validator/validator')
+const { Success } = require('@core/http-exception')
 const { Auth } = require('@middlewares/auth')
 const { generateToken } = require('@core/util')
 const router = new Router({
     prefix: '/v1/token'
 })
 router.post('/', async (ctx, next) => {
-  // const v = await new TokenValidator().validate(ctx)
   const v = await new RegisterValidator().validate(ctx)
   const userParams = {
     username: v.get('body.username'),
@@ -20,14 +20,26 @@ router.post('/', async (ctx, next) => {
   }
   const token = generateToken(user._id, Auth.USER) // 返回token令牌
   ctx.body = {
+    error_code: 0,
     token
   }
 })
 
 router.post('/verify', async (ctx) => {
-  const v = await new NotEmptyValidator().validate(ctx)
-  const result = Auth.verifyToken(v.get('body.token'))
+  const v = await new RegisterValidator().validate(ctx)
+  const userParams = {
+    username: v.get('body.username'),
+    password: v.get('body.password')
+  }
+  console.log('userParams', userParams)
+  const user = await User.getUser(userParams)
+  if (!user.length) {
+    throw new Success('用户名或密码错误', -1)
+  }
+  const v1 = await new NotEmptyValidator().validate(ctx)
+  const result = Auth.verifyToken(v1.get('body.token'))
   ctx.body = {
+    error_code: 0,
     isValid: result
   }
 })
